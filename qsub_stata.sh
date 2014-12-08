8abd121f928ee1d74626a8ec81ddc319fbabbc0b
lionup <lionup@gmail.com>
2014-12-08 18:27:29 +0000

    d

---
+++ qsub_stata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -cwd 				
+#$ -o stata.sgelog
+#$ -q batch.q
+#$ -j y
+#$ -N stata_ran
+#$ -S /bin/bash
+
+. /etc/profile.d/modules.sh
+module load stata/1-wrapper
+echo "loaded modules"
+module list
+
+stata --qsub -b do  impute.do
