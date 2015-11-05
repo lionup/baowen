80a0f6058beed5a62440bf4c3f80627ed2f156d1
lionup@gmail.com <lionup@gmail.com>
2015-11-05 10:24:41 -0500

    d

--- hpc.r
+++ hpc.r
@@ -9,15 +9,26 @@ library(lme4)
 
 load("com.RData")
 
+str(com)
 com$newid<-factor(com$newid)
 com$newhhid<-factor(com$newhhid)
 com$communityid<-factor(com$communityid)
 
+com$lastalcohol<-factor(com$lastalcohol)
+com$lastsmoking<-factor(com$lastsmoking)
 
-pssbp <- glmmadmb(lastactivity~part1+part2+retireage+birthcohort+agewave+
-                              lastbmi+lastalcohol+lastsmoking+
-                              lastincome+
-                              urban+provinces+education+skills+spstatus
-                              +(1|communityid/newhhid/newid),data=com,
-                              zeroInflation=TRUE,family="poisson")
-   
+com$skills<-factor(com$skills)
+com$lastactivity <-factor(com$lastactivity )
+com$anti<-factor(com$anti)
+com$urban<-factor(com$urban)
+com$education<-factor(com$education)
+
+############################mixed regressions
+back<-as.mids(com)
+is.mids(back)
+
+comsbp <- pool(with (back,lmer(sbp ~ part1+part2+retireage+birthcohort+agewave+
+                                  lastbmi+lastalcohol+lastsmoking+
+                                  lastincome+lastactivity+
+                                  urban+provinces+education+skills+spstatus+
+                                (1|communityid/newhhid/newid))))
