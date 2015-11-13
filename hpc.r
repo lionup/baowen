11103bdc82cd5c824515baeb9312dc2a3687bb62
lionup@gmail.com <lionup@gmail.com>
2015-11-13 15:18:12 -0500

    d

--- hpc.r
+++ hpc.r
@@ -7,28 +7,6 @@ library(glmmADMB)
 library(ggplot2)
 library(lme4)
 
-load("com.RData")
-
-str(com)
-com$newid<-factor(com$newid)
-com$newhhid<-factor(com$newhhid)
-com$communityid<-factor(com$communityid)
-
-com$lastalcohol<-factor(com$lastalcohol)
-com$lastsmoking<-factor(com$lastsmoking)
-
-com$skills<-factor(com$skills)
-com$lastactivity <-factor(com$lastactivity )
-com$anti<-factor(com$anti)
-com$urban<-factor(com$urban)
-com$education<-factor(com$education)
-
-############################mixed regressions
-back<-as.mids(com)
-is.mids(back)
-
-comsbp <- pool(with (back,lmer(sbp ~ part1+part2+retireage+birthcohort+agewave+
-                                  lastbmi+lastalcohol+lastsmoking+
-                                  lastincome+lastactivity+
-                                  urban+provinces+education+skills+spstatus+
-                                (1|communityid/newhhid/newid))))
+load('pre.dat')
+imputations <- mice(mdf,meth = meth,pred = pred,maxit = 20, m = 25,seed = 71152)
+save(imputations,file='imputations.dat')
