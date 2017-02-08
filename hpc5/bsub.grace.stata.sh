b633c63c5ea6928cb4a1d04450723f6a66bad05d
ran <lionup@gmail.com>
2017-02-08 10:32:36 +0000

    d

---
+++ hpc5/bsub.grace.stata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#BSUB -q cowles
+#BSUB -W 24:00
+#BSUB -J hpc5
+
+#BSUB -n 20
+#BSUB -R "span[hosts=1]"
+
+#BSUB -o grace.output.%J   # output file name in which %J is replaced by the job ID
+#BSUB -N                   # sent email
+
+# 9. load modules
+module load Apps/Stata/13.1
+
+stata-mp -b do hpc5.do
